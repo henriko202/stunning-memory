3b7959e30f4bd08fe130bf601345c0adfac791b0
Henriko Alberton <henriko.foz@gmail.com>
2024-11-23 18:59:45 -0300

    feat: add search to backend

--- server/medusa-config.ts
+++ server/medusa-config.ts
@@ -40,5 +40,23 @@ module.exports = defineConfig({
         },
       },
     },
+    {
+      resolve: "@rokmohar/medusa-plugin-meilisearch",
+      options: {
+        config: {
+          host: process.env.MEILISEARCH_HOST ?? "",
+          apiKey: process.env.MEILISEARCH_API_KEY ?? "",
+        },
+        settings: {
+          products: {
+            indexSettings: {
+              searchableAttributes: ["title", "description", "variant_sku"],
+              displayedAttributes: ["title", "description", "variant_sku", "thumbnail", "handle"],
+            },
+            primaryKey: "id",
+          },
+        },
+      },
+    },
   ],
 })
